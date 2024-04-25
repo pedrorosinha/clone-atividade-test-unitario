322ea5463ab5540e2b58f1f34f8dac9e8d3baabd
unknown <db@DBS-1162.dbserver.com.br>
2024-04-25 11:23:56 -0300

    test: teste exception não salvar pessoa

--- atividade/src/test/java/com/example/atividade/ServiceTests.java
+++ atividade/src/test/java/com/example/atividade/ServiceTests.java
@@ -1,5 +1,7 @@
 package com.example.atividade;
 
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,10 +11,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
+import com.example.atividade.exceptions.CreatePersonException;
 import com.example.atividade.models.Person;
 import com.example.atividade.repository.PersonRepository;
 import com.example.atividade.services.PersonService;
@@ -27,15 +34,21 @@ public class ServiceTests {
     @InjectMocks
     private PersonService personService;
 
-    @Test
-    @DisplayName("Teste mockado para criar pessoa")
-    void createPerson() {
-        Person person = new Person(
+    Person person;
+
+    @BeforeEach
+    void setup() {
+            person = new Person(
             "Aurora",
             "Kruschewsky",
             "Aurora@email.com.br",
             "Uberlândia - Minas Gerais - Brasil",
             "aaaaa");
+    }
+
+    @Test
+    @DisplayName("Teste mockado para criar pessoa")
+    void createPerson() {
 
         when(personRepository.findByEmail(person.getEmail())).thenReturn(Optional.empty());
         when(personRepository.save(person)).thenReturn(person);
@@ -47,5 +60,18 @@ public class ServiceTests {
         assertEquals(person.getLastName(), savedPerson.getLastName());
         assertEquals(person.getEmail(), savedPerson.getEmail());
         assertEquals(person.getAddress(), savedPerson.getAddress());
+    }
+
+    @Test
+    @DisplayName("Teste mockado para não salvar pessoa")
+    void CreatePersonException() {
+
+        when(personRepository.findByEmail(person.getEmail())).thenReturn(Optional.of(person));
+        assertThrows(CreatePersonException.class, ()->{
+            personService.create(person);
+        });
+
+
+        verify(personRepository, never()).save(any(Person.class));
     }
 }
